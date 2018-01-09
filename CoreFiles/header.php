29c96576b944063a78d8ba40d244378ba2319d82
KristinaMaki5172 <kristinamaki987@gmail.com>
2018-01-08 22:02:37 -0500

    Changes to the header, got some bootstrap

---
+++ CoreFiles/header.php
@@ -0,0 +1,20 @@
+<header>
+    <div class="container">
+        <h2>Hello World</h2>
+    </div>
+    
+    <nav class="navbar" role="navigation">
+        <div>
+        <ul class="nav nav-tabs" role="tablist">
+            <li><a href="home.php">Home</a></li>
+            <li><a href="home.php">Services</a></li>
+            <li><a href="home.php">About Us</a></li>
+            <li><a href="home.php">Projects</a></li>
+            <li><a href="home.php">Pricing</a></li>
+            <li><a href="home.php">Contact</a></li>
+        </ul>
+        </div>
+    </nav>
+    
+<link href="../Resources/bootstrap-3.3.7/dist/css/bootstrap.css" rel="stylesheet">
+</header>
---
+++ CoreFiles/home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+   <meta charset="utf-8">
+   <title>Hello World Tutoring</title>
+</head>
+<body>
+    <?php include "header.php"; ?>
+    
+    <h1>Hello World Tutoring</h1>
+    
+  
+</body>
+    
+<link href="../Resources/bootstrap-3.3.7/dist/css/bootstrap.css" rel="stylesheet">
+</html>
